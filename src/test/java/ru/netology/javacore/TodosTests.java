d5b4d05609e4ece35340a0a7c156102220280421
Omen1984 <ivan.plehanov42@yandex.ru>
2022-05-15 13:08:19 +0500

    Add UnitTests

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,59 @@
 package ru.netology.javacore;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class TodosTests {
 
-    // ваши тесты для класса Todos
+    Todos sut;
+
+    @BeforeEach
+    public void init() {
+        sut = new Todos();
+    }
+
+    @Test
+    public void testTodos_add() {
+        String task1 = "Погулять с собакой";
+        String task2 = "Выучить JavaCore";
+        String expected = "Выучить JavaCore Погулять с собакой";
+
+        sut.addTask(task1);
+        sut.addTask(task2);
+        String result = sut.getAllTasks();
+
+        assertThat(result, equalTo(expected));
+    }
+
+    @Test
+    public void testTodos_remove() {
+        String task1 = "Погулять с собакой";
+        String task2 = "Выучить JavaCore";
+        String expected = "Выучить JavaCore";
+
+        sut.addTask(task1);
+        sut.addTask(task2);
+        sut.removeTask("Погулять с собакой");
+        String result = sut.getAllTasks();
+
+        assertThat(result, equalTo(expected));
+    }
+
+    @Test
+    public void testTodos_getAllTasks() {
+        String task1 = "Пробежка";
+        String task2 = "Акробатика";
+        String task3 = "Учёба";
+        String expected = "Акробатика Пробежка Учёба";
+
+        sut.addTask(task1);
+        sut.addTask(task2);
+        sut.addTask(task3);
+        String result = sut.getAllTasks();
+
+        assertThat(result, equalTo(expected));
+    }
 }
