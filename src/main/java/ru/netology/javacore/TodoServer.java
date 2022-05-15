ff02e35334260eca87a791392968470cd569413e
Omen1984 <ivan.plehanov42@yandex.ru>
2022-05-15 14:49:13 +0500

    Server side development and update maven-surefire-plugin

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,21 +1,55 @@
 package ru.netology.javacore;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private int port;
+    Todos todos;
+    Gson gson;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
+        this.gson = new GsonBuilder().create();
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
+
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            final String inputJsonTask = in.readLine();
+
+            final JsonObject root = JsonParser.parseString(inputJsonTask).getAsJsonObject();
+
+            String type = root.get("type").toString().replaceAll("\"", "");
+            String task = root.get("task").toString().replaceAll("\"", "");
+
+            switch (type) {
+                case "ADD":
+                    todos.addTask(task);
+                    break;
+                case "REMOVE":
+                    todos.removeTask(task);
+                    break;
+            }
+
+            out.println("Задачи: " + todos.getAllTasks());
+        }
     }
 }
