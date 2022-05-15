d821ef54331b01752f139adb50b087a46a2bba58
Omen1984 <ivan.plehanov42@yandex.ru>
2022-05-15 12:35:17 +0500

    Development class Todos

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,18 +4,24 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    List<String> todos;
+    
+    public Todos() {
+        todos = new ArrayList<>();
+    }
 
     public void addTask(String task) {
-        //...
+        todos.add(task);
     }
 
     public void removeTask(String task) {
-        //...
+        todos.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+        return todos.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.joining(" "));
     }
 
 }
